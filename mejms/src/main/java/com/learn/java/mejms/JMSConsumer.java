3db58db4dae6cccce9f3ea2a346556a737842b63
asunwei <18655519621@163.com>
2017-09-18 23:27:10 +0800

    modify finally.

--- mejms/src/main/java/com/learn/java/mejms/JMSConsumer.java
+++ mejms/src/main/java/com/learn/java/mejms/JMSConsumer.java
@@ -3,64 +3,72 @@ package com.learn.java.mejms;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.MessageConsumer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
+
 /**
- * 消息的消费者（接收者）
- * 输入http://127.0.0.1:8161/admin/ 
+ * 消息的消费者（接收者） 输入http://127.0.0.1:8161/admin/
+ * 
  * @author sunwei
  *
  */
 public class JMSConsumer {
-	//默认的连接用户名
+	// 默认的连接用户名
 	private static final String USERNAME = ActiveMQConnection.DEFAULT_USER;
-	//默认连接密码
+	// 默认连接密码
 	private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
-	//默认连接地址
+	// 默认连接地址
 	private static final String BROKEURL = ActiveMQConnection.DEFAULT_BROKER_URL;
-	
+
 	public static void main(String[] args) {
-		//连接工厂
+		// 连接工厂
 		ConnectionFactory connectionFactory;
-		//连接
+		// 连接
 		Connection connection = null;
-		//会话接手或者发送消息的线程
+		// 会话接手或者发送消息的线程
 		Session session;
-		//消息的目的地
+		// 消息的目的地
 		Destination destination;
-		//消息的消费者
+		// 消息的消费者
 		MessageConsumer messageConsumer;
-		//实例化连接工厂
-		connectionFactory = new ActiveMQConnectionFactory(JMSConsumer.USERNAME,JMSConsumer.PASSWORD,JMSConsumer.BROKEURL);
+		// 实例化连接工厂
+		connectionFactory = new ActiveMQConnectionFactory(JMSConsumer.USERNAME, JMSConsumer.PASSWORD,
+				JMSConsumer.BROKEURL);
 		try {
-			//通过创建工厂获取连接
+			// 通过创建工厂获取连接
 			connection = connectionFactory.createConnection();
-			//启动连接
+			// 启动连接
 			connection.start();
-			//创建session
+			// 创建session
 			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			//创建一个连接messHelloWorld的消息队列
+			// 创建一个连接messHelloWorld的消息队列
 			destination = session.createQueue("HelloWorld");
-			//创建消息的消费者
+			// 创建消息的消费者
 			messageConsumer = session.createConsumer(destination);
-			while(true) {
+			while (true) {
 				TextMessage textMessage = (TextMessage) messageConsumer.receive();
-				if(textMessage != null){
-	                   System.out.println("收到的消息:" + textMessage.getText());
-	               }else {
-	                   break;
-	               }
+				if (textMessage != null) {
+					System.out.println("收到的消息:" + textMessage.getText());
+				} else {
+					break;
+				}
 			}
-			
-		} catch(Exception e) {
-			
+
+		} catch (Exception e) {
+
 		} finally {
-			
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-				
 	}
 }
